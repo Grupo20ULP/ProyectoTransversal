8528a822a3530e52a60edb4d1f10d4de39d09d29
Tomas Abatedaga Biole <abatedagatomas@gmail.com>
2025-09-29 20:19:41 -0300

    creo proyecto

---
+++ ProyectoTransversal/src/AccesoDatos/Conexion.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AccesoDatos;
+
+import com.sun.jdi.connect.spi.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author abate
+ */
+public class Conexion {
+    
+    private static final String URL="jdbc:mariadb://localhost:3306/";
+    private static final String DB="GP20uni";
+    private static final String USUARIO="root";
+    private static final String CLAVE="";
+    private static Connection conexion;
+
+    public Conexion() {
+    }
+    
+    public static Connection getConectar(){
+        if(conexion == null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion=(Connection) DriverManager.getConnection(URL+DB,USUARIO,CLAVE);
+                JOptionPane.showMessageDialog(null, "Se conectó");
+                
+            } catch (ClassNotFoundException e) {
+                JOptionPane.showMessageDialog(null, "Problemas accediendo a la clase "+e.getMessage());
+            } catch (SQLException ex){
+                JOptionPane.showMessageDialog(null, "Error al conectar a la base de datos "+ex.getMessage());
+            }
+        }
+        return conexion;
+    }
+}
