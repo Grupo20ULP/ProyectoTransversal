080a112c8d779e6dcba56118ddd65070858d5b7f
Heber Gomez <heber12398@gmail.com>
2025-10-06 19:45:54 -0300

    cambio nombres de carpetas

--- ProyectoTransversal/src/Persistencia/AlumnoData.java
+++ ProyectoTransversal/src/Persistencia/AlumnoData.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package AccesoDatos;
+package Persistencia;
 
-import Entidades.Alumno;
+import Modelo.Alumno;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
