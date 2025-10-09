908da766c3932fa04b03c265f9b837985a3025d1
Federico Galan <tumachgalan14@gmail.com>
2025-10-09 20:51:56 -0300

    test

--- ProyectoTransversal/src/Vistas/MenuPrincipal.java
+++ ProyectoTransversal/src/Vistas/MenuPrincipal.java
@@ -17,7 +17,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
         initComponents();
         setLocationRelativeTo(null);
     }
-
+ 
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
